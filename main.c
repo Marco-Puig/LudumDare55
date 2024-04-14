197fe9a201597d19b2dc77c11eaccefff50608c8
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-04-14 07:12:00 -0400

    New Sprites and Updated Enemy Logic

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main(void)
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1280;
-    const int screenHeight = 720;
+    const int screenHeight = 720; 
 
     InitWindow(screenWidth, screenHeight, "S-Pull");
 
@@ -57,6 +57,12 @@ int main(void)
     int r = rand() % (2 + 1 - 0) + 0; // 2 = max, 0 = min
     
     // globals
+    
+    // Load Floor Sprite/Texture
+    Texture2D floor = LoadTexture("resources/dungeon.png");
+    
+    // Load Enemy Sprite/Texture
+    Texture2D enemySprite = LoadTexture("resources/spike-ball.png");
     
     // Animation variables
     Texture2D spriteSheet = LoadTexture("resources/wizard_idle.png");
@@ -170,7 +176,7 @@ int main(void)
                     if (enemies[i].active) {
                         // Simple AI to follow the player
                         enemies[i].position.x = Lerp(enemies[i].position.x, playerPos.x, (0.005 * r) + 0.005);
-                        enemies[i].position.y = Lerp(enemies[i].position.y, playerPos.y, (0.005 * r) + 0.005);
+                        enemies[i].position.y = Lerp(enemies[i].position.y, playerPos.y + (30 * r), (0.005 * r) + 0.005);
                     }
                 }
                                 
@@ -188,9 +194,10 @@ int main(void)
                     for (int i = 0; i < MAX_BULLETS; i++) {
                         if (!bullets[i].active) {
                             bullets[i].active = true;
-                            bullets[i].position = playerPos;
+                            bullets[i].position = (Vector2){playerPos.x + 120, playerPos.y + 45};
                             bullets[i].velocity = (Vector2){5.0f, 0}; 
-                            break;
+                            break; // break since we want to do one bullet at a time
+                            // we are doing a for loop because we want to check all the bullets in mem that arent active 
                         }
                     }
                 }
@@ -231,14 +238,25 @@ int main(void)
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
                     // reset positions
-                    playerPos.x = (float)screenWidth/2; 
-                    playerPos.y = (float)screenHeight/2;
-
+                    playerPos.x = screenWidth / 2 - frameWidth / 2; 
+                    playerPos.y = screenHeight/ 2 - frameHeight / 2;
+                    
                     score = 0;
                     manaCount = 0;
                     
                     // go back to menu screen
                     currentScreen = TITLE;
+                    
+                    // recalc rng
+                    r = rand() % (2 + 1 - 0) + 0;
+                    
+                    // reset enemy pos
+                    for (int i = 0; i < MAX_ENEMIES; i++) {
+                        enemies[i].position = (Vector2){
+                        GetRandomValue(screenWidth - 100, screenWidth),
+                        GetRandomValue(screenHeight - 100, screenHeight)};
+                        enemies[i].active = true;
+                    }
                 }
             } break;
             default: break;
@@ -268,7 +286,20 @@ int main(void)
 
                 } break;
                 case GAMEPLAY:
-                {                  
+                {                
+                    // Draw Floor  
+                    DrawTextureEx(floor, (Vector2){
+                    screenWidth/2 - floor.width*1.7, 
+                    screenHeight/2 - floor.height*1.5
+                    }, 0.0f, 3.5f, WHITE);
+                    
+                    // Draw Enemies
+                    for (int i = 0; i < MAX_ENEMIES; i++) {
+                        if (enemies[i].active) {
+                            DrawTextureEx(enemySprite, enemies[i].position, 0.0f, 0.3f, WHITE);
+                        }
+                    }  
+                    
                     // Draw Player
                     
                     // Calculate the position of the rectangle on the spritesheet
@@ -279,23 +310,16 @@ int main(void)
                     // Draw the current frame
                     DrawTextureRec(spriteSheet, frameRec, playerPos, WHITE);
                     
-                    // Draw Enemies
-                    for (int i = 0; i < MAX_ENEMIES; i++) {
-                        if (enemies[i].active) {
-                            DrawCircleV(enemies[i].position, 15, RED);
-                        }
-                    }
-                    
-                    // HUD
-                    DrawText(TextFormat("SCORE: %i", score), 20, 20, 40, BLACK);
-                    DrawText(TextFormat("Mana: %i", manaCount), 20, 100, 40, BLUE);
-                    
                     // Draw bullets
                     for (int i = 0; i < MAX_BULLETS; i++) {
                         if (bullets[i].active) {
                             DrawCircleV(bullets[i].position, 5, PURPLE);
                         }
-                    }
+                    }                    
+                   
+                    // HUD
+                    DrawText(TextFormat("SCORE: %i", score), 20, 20, 40, WHITE);
+                    DrawText(TextFormat("Mana: %i", manaCount), 20, 100, 40, BLUE);
 
                     
                 } break;
@@ -320,9 +344,11 @@ int main(void)
 
     // De-Initialization
 
-    // TODO: Unload all loaded data (textures, fonts, audio) here!
+    // Unload all loaded data (textures, fonts, audio) here!
     UnloadSound(music);
     UnloadTexture(spriteSheet);
+    UnloadTexture(enemySprite);
+    UnloadTexture(floor);
     CloseAudioDevice();
 
     CloseWindow(); // Close window and OpenGL context
